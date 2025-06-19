b30968221b0419b9402596a7fbdd3c13e8aef031
Arpit_Joshi <134071763+arpitjoshi24@users.noreply.github.com>
2025-06-19 21:58:04 +0530

    Fix Vercel deployment errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-// src/index.js or src/main.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
-
-
